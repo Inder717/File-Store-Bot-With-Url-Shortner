41382976b3c66eba2195c06a7a6df6ed572302f0
Inder717 <bet.nil2024@gmail.com>
2024-07-01 00:01:02 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,11 +1,11 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
+  API_ID = int(os.environ.get("23929647", ""))
+  API_HASH = os.environ.get("b9afa697042d998a758e407b84c86daf", "")
+  BOT_TOKEN = os.environ.get("7433901506:AAHeqRgAeKhWsFLesFbwzL0kXjQnrsuPrxc", "")
+  BOT_USERNAME = os.environ.get("chhamia_bot", "")
+  DB_CHANNEL = "-1001535343753", ""))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
